7dc68da5ca8f079c7baf7ba78b8032f5b186ddfa
irinaGeani <irina-geanina.corniciuc@student.tuiasi.ro>
2026-05-13 11:45:56 +0300

    Am adaugat procesarea de date de la senzori"

---
+++ server/src/main/java/ro/tuiasi/ac/server/ProcessingSensor.java
@@ -0,0 +1,88 @@
+package ro.tuiasi.ac.server;
+
+import ro.tuiasi.ac.common.*;
+
+public class ProcessingSensor {
+	private final double lidarThresh = 5;
+	private final int redThresh = 200; // un pixel este rosu daca red> 200 green < 50, blue < 50
+	public Command processSensorDataSet(SensorDataSet data) {
+		double[][] leftLidarVals = data.leftLidarFrame().distancesCm();
+		double[][] rightLidarVals = data.rightLidarFrame().distancesCm();
+		
+		 boolean obstacleLeft = false;
+	     boolean obstacleRight = false;
+
+	        // verificare stanga
+	        for(int i = 0; i < leftLidarVals.length; i++) {
+	            for(int j = 0; j < leftLidarVals[i].length; j++) {
+
+	                if(leftLidarVals[i][j] < lidarThresh) {
+	                    obstacleLeft = true;
+	                }
+	            }
+	        }
+
+	        // verificare dreapta
+	        for(int i = 0; i < rightLidarVals.length; i++) {
+	            for(int j = 0; j < rightLidarVals[i].length; j++) {
+
+	                if(rightLidarVals[i][j] < lidarThresh) {
+	                    obstacleRight = true;
+	                }
+	            }
+	        }
+	        
+	     // verificare in stang
+//	        for(int i = 0; i < leftLidarVals.length; i++) {
+//	            for(int j = 0; j < leftLidarVals[i].length; j++) {
+//
+//	                if(leftLidarVals[i][j] < lidarThresh) {
+//	                    obstacleLeft = true;
+//	                }
+//	            }
+//	        }
+
+	        // decizie
+	        if(obstacleLeft) {
+	            return new MoveCommand(MoveDirection.RIGHT, 10);
+	        }
+
+	        if(obstacleRight) {
+	            return new MoveCommand(MoveDirection.LEFT, 10);
+	        }
+	        
+	       
+    		int[][] red = data.cameraFrame().red();
+    		int[][] green = data.cameraFrame().green();
+    		int[][] blue = data.cameraFrame().blue();
+    		
+    		int width = data.cameraFrame().width();
+    		int height = data.cameraFrame().height();
+    		
+    		for(int j = 0; j< height; j++) {
+    			for(int i = 0; i< width; i++) {
+    				if(red[j][i] > redThresh && green[j][i] < 50 && blue[j][i] < 50) {
+    					int targetX = i;
+    					int targetY = j;
+    					if(targetX + 10 < width/2) {
+    						return new RotateCommand(RotateDirection.LEFT, 10);
+    					}else if(targetX - 10 > width/2) {
+    						return new RotateCommand(RotateDirection.RIGHT, 10);
+    					}else if(targetY - 10 < height) {
+    						return new MoveCommand(MoveDirection.FRONT, 10);
+    					}else {
+    						System.out.println("Target reached.");
+    						return new StopCommand();
+    					}
+    				}
+    					
+    			}
+    		}
+	    		
+	    	
+
+	        return new MoveCommand(MoveDirection.FRONT, 10);
+	    }
+	
+	
+}
